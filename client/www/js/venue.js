9cb1d8631691e63f83461f7648bba59c1897cdbf
Rahul Rajan <rahul.rajanus@gmail.com>
2017-09-09 11:37:43 -0400

    hm

---
+++ client/www/js/venue.js
@@ -0,0 +1,21 @@
+function submitVenue(){
+  $('#venueButton').click(function() {
+     function addVenue() {
+      $.ajax({
+        type: 'POST',
+        url: 'https://auth.bazooka69.hasura-app.io/query',
+        contentType: 'application/json',
+        data: {
+          {
+          username: $(".venue-form #venue_name").val(),
+          password: $(".venue-form #password").val()
+          
+        },
+        dataType: 'json'
+    }).done(function(data) {
+        console.log(data)
+        transition()
+      });
+    };
+  });
+}
