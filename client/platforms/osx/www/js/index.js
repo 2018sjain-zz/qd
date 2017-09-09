2c0316cb2db2969e01ed58c96f4dc67b5fa1c881
saarthak24 <saarthaksethi2008@gmail.com>
2017-09-09 11:33:16 -0400

    not good

--- client/platforms/osx/www/js/index.js
+++ client/platforms/osx/www/js/index.js
@@ -2,7 +2,7 @@ function onLoad() {
   document.addEventListener("deviceready", onDeviceReady, true);
 }
 
-function goToVenue(){
+function goToVenue() {
   window.location = "venue.html";
 }
 
@@ -18,21 +18,21 @@ $('.message a').click(function() {
 });
 
 $('#registerButton').click(function() {
-   function registerUser() {
-    $.ajax({
-      //type: 'POST',
-      url: 'https://auth.bazooka69.hasura-app.io/signup',
-      contentType: 'application/json',
-      authorization: 'vcd0bp4a8s9lqlcppyiokodg9ber69dx',
-      data: {
-        username: $(".register-form #username").val(),
-        password: $(".register-form #password").val()
-      },
-      dataType: 'json'
+  $.ajax({
+    type: 'POST',
+    url: 'https://auth.bazooka69.hasura-app.io/signup',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer vcd0bp4a8s9lqlcppyiokodg9ber69dx'
+    },
+    data: {
+      username: $(".register-form #username").val(),
+      password: $(".register-form #password").val()
+    }
   }).done(function(data) {
-      transition()
-    });
-  };
+    console.log(data)
+    transition()
+  });
 });
 
 $('#loginButton').click(function() {
