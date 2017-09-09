e75cdc31bf5d9bf530e1ae0e5640476b1b2f200d
Rahul Rajan <rahul.rajanus@gmail.com>
2017-09-09 12:39:50 -0400

    working temp

--- client/platforms/browser/www/js/index.js
+++ client/platforms/browser/www/js/index.js
@@ -34,21 +34,27 @@ $('.message a').click(function() {
 });
 
 $('#registerButton').click(function() {
-  $.ajax({
-    type: 'POST',
-    url: 'https://auth.bazooka69.hasura-app.io/signup',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer vcd0bp4a8s9lqlcppyiokodg9ber69dx'
-    },
-    data: {
-      username: $(".register-form #username").val(),
-      password: $(".register-form #password").val()
-    }
-  }).done(function(data) {
-    console.log(data)
-    transition()
-  });
+  // $.ajax({
+  //   type: 'POST',
+  //   url: 'https://auth.bazooka69.hasura-app.io/signup',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': 'Bearer vcd0bp4a8s9lqlcppyiokodg9ber69dx'
+  //   },
+  //   data: {
+  //     username: $(".register-form #username").val(),
+  //     password: $(".register-form #password").val()
+  //   }
+  // }).done(function(data) {
+  //   console.log(data)
+  //   transition()
+  // });
+  hasura.user
+  hasura.setUsername($(".register-form #username").val());
+  hasura.auth.login($(".register-form #password").val(), onSuccess, onError);
+  hasura.user
+  console.log(hasura.user)
+  transition()
 });
 
 $('#loginButton').click(function() {
