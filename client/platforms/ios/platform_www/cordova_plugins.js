41e61499b0a3c912e8c329d78398bcc1ad563866
Rahul Rajan <rahul.rajanus@gmail.com>
2017-09-09 22:22:57 -0400

    holy shit this is annoying

--- client/platforms/ios/platform_www/cordova_plugins.js
+++ client/platforms/ios/platform_www/cordova_plugins.js
@@ -7,16 +7,43 @@ module.exports = [
         "clobbers": [
             "QRScanner"
         ]
+    },
+    {
+        "id": "org.apache.cordova.dialogs.notification",
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "pluginId": "org.apache.cordova.dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "pluginId": "org.apache.cordova.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-browsersync": "0.1.1",
     "cordova-plugin-compat": "1.1.0",
     "cordova-plugin-swift-support": "3.1.1",
     "cordova-plugin-qrscanner": "2.5.0",
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-browsersync": "0.1.1"
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "phonegap-plugin-barcodescanner": "4.0.2"
 };
 // BOTTOM OF METADATA
 });
--- client/www/js/nearby.js
+++ client/www/js/nearby.js
@@ -6,10 +6,22 @@ function get_venues(){
       columns: ['*']
     }},
     (data) => { console.log(data);
+      venue_json = data
+      for(var i = 0;i<venue_json.length;i++)
+      {
+        $("#fillin_venue").append("<li class=\"list-item list-item--tappable\" ><div class=\"list-item__center\">" + venue_json[i].venue_name + "</div><div class=\"list-item__right\"><div class=\"list-item__label\">" + venue_json[i].vicinity + " mile(s) away</div></div>  </li>")
+      }
       return data
     },
     (error) => { console.log(error);
 
     }
   );
+}
+function inject_venues(){
+  x = get_venues()
+  for(var i = 0;i<x.length;i++)
+  {
+
+  }
 }
