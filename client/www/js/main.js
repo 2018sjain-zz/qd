7a04cb3e39ebf9c51f4ea8404fc47ed265ac9d8f
saarthak24 <saarthaksethi2008@gmail.com>
2017-09-10 01:43:24 -0400

    make links work

---
+++ client/www/js/main.js
@@ -0,0 +1,23 @@
+$('.tab-bar__button').click(function() {
+  event.preventDefault();
+});
+
+function goToNearby() {
+    window.location = "nearby.html";
+}
+
+function goToSearch() {
+    window.location = "search.html";
+}
+
+function goToQ() {
+    window.location = "q.html";
+}
+
+function goToVenue() {
+    window.location = "venue.html";
+}
+
+function goToProfile() {
+    window.location = "profile.html";
+}
